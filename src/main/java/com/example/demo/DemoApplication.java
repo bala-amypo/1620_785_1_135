3d6ad558806317ee76365c75f979969230be9b72
bala-amypo <bala@amypo.in>
2025-12-16 06:51:08 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  {
+public class SwaggerConfig {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
