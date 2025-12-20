2c2758f7b1130af3ecef5ac3a67e1c9a658cc2af
bala-amypo <bala@amypo.in>
2025-12-20 21:00:13 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 import java.util.List;
+import com.example.demo.entity.User;
 public interface UserService {
 
     User registerUser(User user);   // check duplicate email
