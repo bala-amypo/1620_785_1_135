27f5dd457ef45ba7a4383321b68f0e632cb436ec
bala-amypo <bala@amypo.in>
2025-12-21 10:06:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Subscription.java
+++ src/main/java/com/example/demo/entity/Subscription.java
@@ -1,23 +1,8 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-
-@Entity
 public class Subscription {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private Event event;
-
-    @ManyToOne
-    private User user;
-
     public Long getId() { return id; }
-    public Event getEvent() { return event; }
-    public void setEvent(Event event) { this.event = event; }
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public void setId(Long id) { this.id = id; }
 }
