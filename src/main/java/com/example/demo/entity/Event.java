5c8a11e90dcf80613bc068386f6fc32cdb9d28a0
bala-amypo <bala@amypo.in>
2025-12-21 09:39:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Event.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private boolean active = true;
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
+}
