5f6c278971d883d6222fdb41a5a86c4b2673deef
bala-amypo <bala@amypo.in>
2025-12-22 04:39:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -14,5 +14,42 @@ public class Event {
 
     private String description;
 
-    public Event() {}
+    private boolean active;
+
+    public Event() {
+    }
+
+    // ===== GETTERS =====
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    // ===== SETTERS =====
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
